5dbe808860ce5f4e415fb024f9c7e815cbe07675
janainarsantos9-sketch <janaina.r.santos9@aluno.senai.br>
2026-09-03 16:54:01 -0300

    Add files via upload

--- FUND_PROGRAMACAO/ConvertendoValores/src/conversao/App.java
+++ FUND_PROGRAMACAO/ConvertendoValores/src/conversao/App.java
@@ -3,8 +3,18 @@ package conversao;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+    	
+    	String numero1 ="10";
+    	String numero2 ="5";
+    	String numero3 ="12.50";
+    	
+    	System.out.println(numero1 + numero2);
+    	
+    	
+    	int n1 = Integer.parseInt()
+    	
+    	
 	}
 
 }
